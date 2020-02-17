d5d07e63c3150a3f8d6bf849d1100e1f6a2f9706
ituski <tf.k.itsuki.rits@ezweb.ne.jp>
2020-02-18 06:02:52 +0900

    修正

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,8 +20,8 @@ const store = createStore(
   initialState,
 
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
